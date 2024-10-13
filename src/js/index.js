bef05d26edc8acae996a16228d88d927028616a9
Henrique <costa.rique8@gmail.com>
2024-10-12 22:50:44 -0300

    Add files via upload

--- src/js/index.js
+++ src/js/index.js
@@ -1,33 +1,37 @@
-/*
-   OBJETIVO - quando clicar no pokedev da listagem temos que esconder o cartão do pokedev aberto 
-   e mostrar o cartão correspondente ao que foi selecionado na listagem
-       PASSO 1 - precisamos criar uma variável no JS pra trabalhar com a listagem de pokedevs
-       PASSO 2 - Identificar o evento de clique no elemento da listagem
-       PASSO 3 - remover a classe aberto só do cartão que tá aberto
-       PASSO 4 - ao clicar em um pokedev da lista pegamos o id desse pokedev pra saber qual 
-    cartão abrir
-       PASSO 5 - remover a classe ativo que marca o pokedev selecionado na listagem
-       PASSO 6 - adicinar a classe ativo no pokedev selecionado na listagem
-*/
 
 const selectionListPokedevs = document.querySelectorAll('.pokedev');
 
 selectionListPokedevs.forEach(pokedev =>{
     pokedev.addEventListener("click",() => {
-        const cartaoPokedevaberto = document.querySelector(".aberto");
-        cartaoPokedevaberto.classList.remove("aberto");
+        hidePokedevCard();
 
-        const idPokedevSelected = pokedev.attributes.id.value;
-        
-        const idPodedevCardToOpen = "cartao-" + idPokedevSelected;
+        const idPokedevSelected = showPokedevCard(pokedev);
 
-        const pokedevCardToOpen = document.getElementById(idPodedevCardToOpen);
-        pokedevCardToOpen.classList.add("aberto");
+        deactivatePokedevOnList();
 
-        const activePokedevOnList = document.querySelector(".ativo");
-        activePokedevOnList.classList.remove("ativo")
-
-        const selectedPokedevOnList = document.getElementById(idPokedevSelected);
-        selectedPokedevOnList.classList.add("ativo")
+        activatePokedevOnList(idPokedevSelected);
     })
 })
+function activatePokedevOnList(idPokedevSelected) {
+    const selectedPokedevOnList = document.getElementById(idPokedevSelected);
+    selectedPokedevOnList.classList.add("active");
+}
+
+function deactivatePokedevOnList() {
+    const activePokedevOnList = document.querySelector(".active");
+    activePokedevOnList.classList.remove("active");
+}
+
+function showPokedevCard(pokedev) {
+    const idPokedevSelected = pokedev.attributes.id.value;
+    const idPodedevCardToOpen = "card-" + idPokedevSelected;
+    const pokedevCardToOpen = document.getElementById(idPodedevCardToOpen);
+    pokedevCardToOpen.classList.add("open");
+    return idPokedevSelected;
+}
+
+function hidePokedevCard() {
+    const pokedevOpenCard = document.querySelector(".open");
+    pokedevOpenCard.classList.remove("open");
+}
+
